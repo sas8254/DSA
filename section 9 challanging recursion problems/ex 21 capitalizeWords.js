2654d535f6d4c66f4dc3089c8cfc62abc68448e3
sas8254 <shashibhushansingh11@gmail.com>
2023-05-15 16:10:29 +0530

    acc ex 21

--- section 9 challanging recursion problems/ex 21 capitalizeWords.js
+++ section 9 challanging recursion problems/ex 21 capitalizeWords.js
@@ -1,4 +1,7 @@
-function capitalizeWords() {}
+function capitalizeWords(arr) {
+  if (arr.length === 0) return [];
+  return [arr[0].toUpperCase(), ...capitalizeWords(arr.slice(1))];
+}
 
 let words = ["i", "am", "learning", "recursion"];
 console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
